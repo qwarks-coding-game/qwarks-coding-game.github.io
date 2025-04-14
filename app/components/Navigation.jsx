59657271b07c59f2bcc030b676cc9987ddb9404c
MatthewWHuang <thematthewhuang@gmail.com>
2025-04-14 18:12:19 -0400

    Sign out on click

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import logo from "../images/logo.png";
 import NavigationElement from "./NavigationElement";
 import { AuthContext } from "@/app/context/AuthContext";
+import { signOut } from "@/utils/firebase-caller";
 
 export default function Navigation() {
     const { user } = useContext(AuthContext);
@@ -14,14 +15,19 @@ export default function Navigation() {
             console.log("User is logged in in nav:", user);
         }
     }, [user]);
-    
+
     return (
         <div style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center", padding: "10px", position: "sticky", top: "0", backgroundColor: "black" }} className={"navbar"}>
             <NavigationElement to="/"><Image src={logo} alt="QWARKS logo" width="50" height="50" /></NavigationElement>
             <NavigationElement to="/">QWARKS</NavigationElement>
             <NavigationElement to="/about">About</NavigationElement>
             <NavigationElement to="/install">Install</NavigationElement>
-            {user ? <p>{user.email}</p> : <NavigationElement to="/login">Login</NavigationElement>}
+            {user ? <p onClick={signOut}>{user.email}</p> :
+                <div style={{display: "flex", flexDirection: "row"}}>
+                    <NavigationElement to="/login">Login</NavigationElement>
+                    <NavigationElement to="/signup">Sign Up</NavigationElement>
+                </div>
+            }
         </div>
     );
 }
